f019d14c636f457e072ae48b4403399b9760059c
RuachStudio <shockthesheepstudios@gmail.com>
2025-10-29 13:57:19 -0500

    server.js now includes the url property so Strapi will generate links using PUBLIC_URL (e.g. https://joinruach.org) instead of http://0.0.0.0:1337.

--- ruach-ministries-backend/config/server.js
+++ ruach-ministries-backend/config/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://joinruach.org'),
   app: {
     keys: env.array('APP_KEYS'),
   },
