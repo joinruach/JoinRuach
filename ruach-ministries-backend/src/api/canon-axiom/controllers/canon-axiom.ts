7a187e88577aa634a5d1ae3f2391a57eacedd5d0
RuachStudio <shockthesheepstudios@gmail.com>
2025-12-30 14:34:20 -0600

    updated

---
+++ ruach-ministries-backend/src/api/canon-axiom/controllers/canon-axiom.ts
@@ -0,0 +1,7 @@
+/**
+ * canon-axiom controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::canon-axiom.canon-axiom');
---
+++ ruach-ministries-backend/src/api/canon-axiom/services/canon-axiom.ts
@@ -0,0 +1,7 @@
+/**
+ * canon-axiom service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::canon-axiom.canon-axiom');
